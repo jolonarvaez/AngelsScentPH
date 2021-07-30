cbc7f76f9ce8bf05db2e0c5f6075c1f811a16bc0
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2021-07-30 18:28:44 +0800

    new project set up

---
+++ components/Navbar.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid header text-uppercase">
+            <a class="navbar-brand" href="#">AngelScentsPH</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item px-2">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Cart</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Payment Form</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Dashboard</a>
+                </li>
+                <li class="nav-item px-2">
+                    <a class="nav-link" href="#">Account</a>
+                </li>
+                
+            </ul>
+            </div>
+        </div>
+    </nav>
+    </div>
+    
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .header{
+        font-size: 12px;
+    }
+</style>
