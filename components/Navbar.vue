dd5e768456a755be193c06feb18033b44db5893a
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2021-08-13 00:00:58 +0800

    updated nav bar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -21,7 +21,7 @@
                     <a class="nav-link" href="#">Payment Form</a>
                 </li>
                 <li class="nav-item px-2">
-                    <a class="nav-link" href="#">Contact Us</a>
+                    <a class="nav-link" href="/contact">Contact Us</a>
                 </li>
                 <li class="nav-item px-2">
                     <a class="nav-link" href="#">Dashboard</a>
