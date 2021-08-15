de2164ebc40d0bae04bebd93a5e553adb4b78c11
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2021-08-15 23:30:29 +0800

    added featured product component

---
+++ components/FeaturedProduct.vue
@@ -0,0 +1,39 @@
+<template>
+    <a href="/product">
+        <div class="card border-0 text-center m-auto" style="width: 10rem;">
+            <img src="~/assets/product.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title text-uppercase medium">Adelaide</h5>
+                <p class="card-text regular">BURBERRY X BURBERRY</p>
+            </div>
+        </div>
+    </a>
+
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.card{
+    background-color: #FAFAFA;
+}
+
+.card-title{
+    font-size: 0.9rem;
+    color: black;
+}
+
+.card-text{
+    color: #79808F;
+    font-size: 0.8rem;
+}
+
+a{
+    text-decoration: none;
+}
+
+</style>
