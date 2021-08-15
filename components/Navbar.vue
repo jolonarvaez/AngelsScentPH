6cc85b24177cc5a186db2de109211e16565bd53c
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2021-08-15 18:53:43 +0800

    updated navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,39 +1,37 @@
 <template>
     <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid header text-uppercase">
-            <a class="navbar-brand" href="/">AngelScentsPH</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item px-2">
-                    <a class="nav-link" aria-current="page" href="/">Home</a>
-                </li>
-                <li class="nav-item px-2">
-                    <a class="nav-link" href="/cart">Cart</a>
-                </li>
-                <li class="nav-item px-2">
-                    <a class="nav-link" href="#">Payment Form</a>
-                </li>
-                <li class="nav-item px-2">
-                    <a class="nav-link" href="/contact">Contact Us</a>
-                </li>
-                <li class="nav-item px-2">
-                    <a class="nav-link" href="#">Dashboard</a>
-                </li>
-                <li class="nav-item px-2">
-                    <a class="nav-link" href="/account">Account</a>
-                </li>
-                
-            </ul>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid header text-uppercase">
+                <a class="navbar-brand" href="/">AngelScentsPH</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item px-2">
+                        <a class="nav-link" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="/cart">Cart</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="#">Payment Form</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="/contact">Contact Us</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="#">Dashboard</a>
+                    </li>
+                    <li class="nav-item px-2">
+                        <a class="nav-link" href="/account">Account</a>
+                    </li>
+                    
+                </ul>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
     </div>
-    
-
 </template>
 
 <script>
