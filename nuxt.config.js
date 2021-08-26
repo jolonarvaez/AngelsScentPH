fd7f48856c8949b4a52813289ec67757ca9e813e
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2021-08-27 00:51:59 +0800

    added vuex-persists module

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/imports.js', mode: 'client' }
+    { src: '~/plugins/imports.js', mode: 'client' },
+    { src: '~/plugins/vuex-persist', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
