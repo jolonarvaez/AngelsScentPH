53e7d44b7c1508bfb7dcb3dd8a933cf38b3a281a
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2021-08-27 01:12:50 +0800

    changed middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 export default function ({ $fire, route, redirect, store }) {
     if(route.path.startsWith('/dashboard') && !store.state.isAdmin){
-        return redirect('/')
+        return redirect('/products')
     }
   }
   
