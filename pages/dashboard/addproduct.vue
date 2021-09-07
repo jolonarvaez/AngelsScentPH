4738a5aefe8632a84c71000b00ba0fb7d864b478
Scoopdiddy-whoop <80666004+Scoopdiddy-whoop@users.noreply.github.com>
2021-09-07 22:33:32 +0800

    completed edit product, added partially completed addproduct

---
+++ pages/dashboard/addproduct.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+        <div class="dashboard-header text-center text-uppercase py-2 regular">
+            Dashboard
+        </div>
+        <div class="container-fluid ">
+            <div class="row py-3">
+                <DashboardNavbar />
+                <div class="col-10">
+                    <div class="shadow container-fluid data-bg p-3">
+                        <div class="table-header medium text-uppercase py-3">
+                                Add product
+                        </div>
+                        <div id="content" class="container-fluid d-flex flex-column regular text-uppercase p-5 w-100 bg justify-content-center">
+                            <div class="row mt-3 pt-2 d-flex">
+                                <div class="col-sm-2">
+                                    <p>Product Name</p>
+                                </div>
+                                <div class="col-sm-4">
+                                    <input class="w-75 border border-secondary" type="text" id="productName" required>
+                                </div>
+                                <div class="col-sm-6"></div>
+                            </div>
+                            <div class="row mt-3 pt-2">
+                                <div class="col-sm-2">
+                                    <p>Product Description</p>
+                                </div>
+                                <div class="col-sm-5">
+                                    <textarea id="productDesc" class="form-control border border-secondary" rows="3"></textarea>
+                                </div>
+                                <div class="col-sm-5"></div>
+                            </div>
+                            <div class="row mt-3 pt-2">
+                                <div class="col-sm-2">
+                                    <p>Length</p>
+                                </div>
+                                <div class="col-sm-4">
+                                    <input class="w-75 border border-secondary" type="text" id="length" required>
+                                </div>
+                                <div class="col-sm-6"></div>
+                            </div>
+                            <div class="row mt-3 pt-2">
+                                <div class="col-sm-2">
+                                    <p>Width</p>
+                                </div>
+                                <div class="col-sm-4">
+                                    <input class="w-75 border border-secondary" type="text" id="width" required>
+                                </div>
+                                <div class="col-sm-6"></div>
+                            </div>
+                            <div class="row mt-3 pt-2">
+                                <div class="col-sm-2">
+                                    <p>Height</p>
+                                </div>
+                                <div class="col-sm-4">
+                                    <input class="w-75 border border-secondary" type="text" id="height" required>
+                                </div>
+                                <div class="col-sm-6"></div>
+                            </div>
+                            <div class="row mt-3 pt-2">
+                                <div class="col-sm-2">
+                                    <p>Weight (In ml)</p>
+                                </div>
+                                <div class="col-sm-4">
+                                    <input class="w-75 border border-secondary" type="text" id="weight" required>
+                                </div>
+                                <div class="col-sm-6"></div>
+                            </div>
+                            <div class="row mt-3 pt-2">
+                                <div class="col-sm-2">
+                                    <p>Price</p>
+                                </div>
+                                <div class="col-sm-4">
+                                    <input class="w-75 border border-secondary" type="text" id="price" required>
+                                </div>
+                                <div class="col-sm-6"></div>
+                            </div>
+                            <div class="row mt-3 pt-2">
+                                <div class="col-sm-2">
+                                    <p>Quantity</p>
+                                </div>
+                                <div class="col-sm-4">
+                                    <input class="w-75 border border-secondary" type="text" id="quantity" required>
+                                </div>
+                                <div class="col-sm-6"></div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <p>Product Photo</p>
+                                </div>
+                                <div class="col-sm-4">
+                                    <input type="file" id="formFile" accept="image/png, image/gif, image/jpeg" required>
+                                </div>
+                                <div class="col-sm-6"></div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-4"></div>
+                                <div class="col-sm-4">
+                                    <button type="submit" class="btn w-75 pt py-2 px-3" id = "submit">Add product</button>
+                                </div>
+                                <div class="col-sm-4"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.btn{
+    background-color: #9F9A96;
+    color: white;
+    border-radius: 8px;
+    font-weight: bold;
+
+}
+</style>
--- pages/editproduct.vue
+++ pages/editproduct.vue
@@ -2,8 +2,8 @@
 <div>
     <div id="contact"><button type="button" class="btn btn-info btn" data-toggle="modal" data-target="#contact-modal">Show Contact Form</button></div>
     <div id="contact-modal" class="modal fade" role="dialog">
-        <div class="modal-dialog modal-xl ">
-            <div class="modal-content">
+        <div class="modal-dialog modal-xl round">
+            <div class="modal-content round">
                 <div class="container-fluid d-flex flex-column regular text-uppercase p-5 w-100 bg">
                     <div class="row">
                         <div class="col-sm-3">
@@ -18,7 +18,7 @@
                             <p>Product Name</p>
                         </div>
                         <div class="col-sm-9">
-                            <input class="w-75" type="text" id="productName" required>
+                            <input class="w-75 border border-secondary" type="text" id="productName" required>
                         </div>
                     </div>
                     <div class="row mt-3 pt-2">
@@ -26,23 +26,31 @@
                             <p>Product Description</p>
                         </div>
                         <div class="col-sm-9">
-                            <textarea id="productDesc" class="form-control" rows="3"></textarea>
+                            <textarea id="productDesc" class="form-control border border-secondary" rows="3"></textarea>
                         </div>
                     </div>
                     <div class="row mt-3 pt-2">
                         <div class="col-sm-3">
-                            <p>Dimensions</p>
+                            <p>Length</p>
                         </div>
                         <div class="col-sm-9">
-                            <input class="w-75" type="text" id="dimensions" required>
+                            <input class="w-75 border border-secondary" type="text" id="length" required>
                         </div>
                     </div>
                     <div class="row mt-3 pt-2">
                         <div class="col-sm-3">
-                            <p>Weight</p>
+                            <p>Width</p>
                         </div>
                         <div class="col-sm-9">
-                            <input class="w-75" type="text" id="weight" required>
+                            <input class="w-75 border border-secondary" type="text" id="width" required>
+                        </div>
+                    </div>
+                    <div class="row mt-3 pt-2">
+                        <div class="col-sm-3">
+                            <p>Height</p>
+                        </div>
+                        <div class="col-sm-9">
+                            <input class="w-75 border border-secondary" type="text" id="height" required>
                         </div>
                     </div>
                     <div class="row mt-3 pt-2">
@@ -50,7 +58,7 @@
                             <p>Weight (In ml)</p>
                         </div>
                         <div class="col-sm-9">
-                            <input class="w-75" type="text" id="weight" required>
+                            <input class="w-75 border border-secondary" type="text" id="weight" required>
                         </div>
                     </div>
                     <div class="row mt-3 pt-2">
@@ -58,7 +66,7 @@
                             <p>Price</p>
                         </div>
                         <div class="col-sm-9">
-                            <input class="w-75" type="text" id="price" required>
+                            <input class="w-75 border border-secondary" type="text" id="price" required>
                         </div>
                     </div>
                     <div class="row mt-3 pt-2">
@@ -66,7 +74,7 @@
                             <p>Quantity</p>
                         </div>
                         <div class="col-sm-9">
-                            <input class="w-75" type="text" id="quantity" required>
+                            <input class="w-75 border border-secondary" type="text" id="quantity" required>
                         </div>
                     </div>
                     <div class="row mt-3 pt-2">
@@ -74,12 +82,16 @@
                             <p>Status</p>
                         </div>
                         <div class="col-sm-3">
-                            <select class="form-select">
+                            <select class="form-select border border-secondary">
                                 <option value="1" selected>LISTED</option>
                                 <option value="2">NOT LISTED</option>
                             </select>
                         </div>
                     </div>
+                    <div class="row mt-3 pt-5 w-100 d-flex justify-content-center">
+                        <button type="submit" class="btn btn-lg btn-block w-75" id = "submit">Save Changes</button>
+                    </div>
+                    
                 </div>
             </div>
 	    </div>
@@ -90,5 +102,13 @@
 <style scoped>
 .bg{
     background-color: #FAFAFA;
+}
+#submit{
+    background-color: #9F9A96;
+    font-weight: bold;
+    color: white;
+}
+.round{
+    border-radius: 30px;
 }
 </style>
