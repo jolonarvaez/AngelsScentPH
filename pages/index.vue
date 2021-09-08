e60376a96a57c3d660c44fef72f5ae6f930c213a
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2021-09-08 11:10:55 +0800

    updated index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,35 @@
 <template>
-  <div class="d-flex flex-column">
-      <div class="row justify-content-between nopadding">
+  <div class="d-flex flex-column h-100 ">
+      <div class="d-flex flex-row h-100">
           <div class="col-lg nopadding img-container">
               <div class="logo_bar w-100 py-2 d-flex justify-content-around"> 
                   <div class="col-sm-3 d-flex align-items-center">
                       <img src="~/assets/logo.png" class="img_bar img-fluid mx-2">
                   </div>
                   
-                  <div class="text-uppercase logo_text">Angel's Scent</div>
+                  <div class="text-uppercase logo_text text-right">Angel's Scent</div>
               </div>
               <img src="~/assets/Flowers.jpg" class="w-100 h-100 left-container">
           </div>
           
           <div class="col-lg nopadding">
-              <div class="right-container d-flex align-items-center">
-                  <div class="container m-4">
-                      <div class="title display-3 my-2">
+              <div class="right-container d-flex align-items-center w-100 h-100">
+                  <div class="container m-5 mt-auto w-100 ">
+                      <div class="title display-3 ">
                           Smell <br> heavenly.
                       </div>
                       <div class="caption mt-4">
                           Try a new fragrance today with Angel’s Scent.
                       </div>
-                      <div class="d-flex justify-content-end mt-3 mx-4">
-                          <NuxtLink to="/products"><button type="button" class="btn btn-light px-4 shop-now">SHOP NOW</button></NuxtLink>
+                      <div class="d-flex justify-content-end m-5">
+                          <NuxtLink to="/products"><button type="button" class="btn btn-lg px-4 shop-now">SHOP NOW</button></NuxtLink>
                       </div>
                   </div>
+
               </div>
           </div>
       </div>
-      <div class="row nopadding">
-        <div class="container-fluid">
-            <div class="text-center text-uppercase mt-4 featured-title">Featured Products</div>
-            <div class="row justify-content-center my-4">
-                <div class="col-sm-2 my-2">
-                    <FeaturedProduct />
-                </div>
-                <div class="col-sm-2 my-2">
-                    <FeaturedProduct />
-                </div>
-                <div class="col-sm-2 my-2">
-                    <FeaturedProduct />
-                </div>
-       
-            </div>
-        </div>
-      </div>
+      
   </div>
 </template>
 
@@ -95,12 +80,15 @@ export default {
   .title{
     color: #7C6565;
     line-height: 80%;
+    font-size: 135px;
+    font-weight: bold;
+    letter-spacing: -0.5px;
   }
 
   .caption{
     color: #7C6565;
-    font-size: 15px;
-    letter-spacing: 0.5px;
+    font-size: 25px;
+    font-family: inter-regular;
   }
 
   br {
@@ -117,6 +105,9 @@ export default {
     background-color: #9F9A96;
     border-color: #9F9A96;
     color: white;
+    font-size:20px;
+    font-family: inter-regular;
+    
   }
 
   .featured-title{
