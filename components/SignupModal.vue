2a72ac8abeb4f83d96813a89ba6265a22036ac53
Dontyell <danny_enriquez@dlsu.edu.ph>
2021-09-14 17:50:49 +0800

    updated signup modal

--- components/SignupModal.vue
+++ components/SignupModal.vue
@@ -1,48 +1,69 @@
 <template>
-
-<div class="modal fade" id="signup" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
+<div class="modal fade" id="signup" tabindex="-1">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
         <div class="modal-content">
-        <div class="modal-header">
-            <div class="d-flex m-auto">
-                <div class="p-2 mx-3">Sign Up</div>
-                <!-- <div class="p-2 mx-3">Log In</div> -->
-            </div>
-        </div>
-        <form ref="registerForm" action="" @submit="submit">
-            <div class="modal-body text-capitalized">
-                <div class="input group">
-                    <div class="row">
-                        <div class="col">
-                            <div class="d-flex justify-content-between">
-                                <input class="form-format me-2 p-1" v-model="fName" type="text" placeholder="First Name" required>
-                                <input class="form-format ms-2 p-1" v-model="lName" type="text" placeholder="Last Name" required>
+            <div class="modal-body">
+
+                <div class="container-fluid border-bottom d-flex justify-content-center medium">
+                    <p class="text-size text-uppercase">Sign Up</p>
+                </div>
+
+                <form ref="registerForm" action="" @submit="submit">
+                    <div class="container-fluid d-flex justify-content-center">
+                        <div class="signup-container my-1">
+                            <div class="input group">
+                                <div class="row">
+                                    <!-- First Half -->
+                                    <div class="col">
+                                        <div class="d-flex justify-content-between">
+                                            <div class="col-md-6">
+                                                <label class="text-size medium text-uppercase me-2 mt-2" for="fName">First Name</label>
+                                                <input class="form-control form-format me-2 mb-2" v-model="fName" type="text" required>
+                                            </div>
+
+                                            <div class="col-md-6">
+                                                <label class="text-size medium text-uppercase ms-2 mt-2" for="lName">Last Name</label>
+                                                <input class="form-control form-format ms-2 mb-2" v-model="lName" type="text" required>
+                                            </div>
+                                        </div>
+
+                                        <label class="text-size medium text-uppercase mt-2" for="contactNo">Contact Number</label>
+                                        <input class="form-control form-format mb-2" v-model="contactNo" type="number" required>
+
+                                        <label class="text-size medium text-uppercase mt-2" for="email">Email</label>
+                                        <input class="form-control form-format mb-2" v-model="email" type="text" requried>
+
+                                        <label class="text-size medium text-uppercase mt-2" for="password">Password</label>
+                                        <input class="form-control form-format mb-2" v-model="password" type="password" required>
+                                    </div>
+
+                                    <!-- Second Half -->
+                                    <div class="col">
+                                        <label class="text-size medium text-uppercase mt-2" for="streetAdd">Street Address</label>
+                                        <input class="form-control form-format mb-2" v-model="streetAdd" type="text" required>
+
+                                        <label class="text-size medium text-uppercase mt-2" for="city">City</label>
+                                        <input class="form-control form-format mb-2" v-model="city" type="text" required>
+
+                                        <label class="text-size medium text-uppercase mt-2" for="province">Province</label>
+                                        <input class="form-control form-format mb-2" v-model="province" type="text" required>
+                                        
+                                        <label class="text-size medium text-uppercase mt-2" for="zipcode">Zipcode</label>
+                                        <input class="form-control form-format mb-2" v-model="zipcode" type="number" required>
+                                    </div>
+                                </div>
+
+                                <!-- Button -->
+                                <div class="container-fluid d-flex justify-content-center mt-4 mb-2">
+                                    <div class="row btn-container">
+                                        <button type="submit" class="btn btn-secondary btn-format text-uppercase">Register</button>
+                                    </div>
+                                </div>
                             </div>
-                        
-                            <input class="form-format w-100 my-2 p-1" v-model="contactNo" type="number" placeholder="Contact Number" required>
-                            
-                            <input class="form-format w-100 my-2 p-1" v-model="email" type="text" placeholder="Email" requried>
-                           
-                            <input class="form-format w-100 my-2 p-1" v-model="password" type="password" placeholder="Password" required>
-                            
-                        </div>
-                        <div class="col">
-                            <input class="form-format w-100 mb-2 p-1" v-model="streetAdd" type="text" placeholder="Street Address" required>
-                            
-                            <input class="form-format w-100 my-2 p-1" v-model="city" type="text" placeholder="City" required>
-                            
-                            <input class="form-format w-100 my-2 p-1" v-model="province" type="text" placeholder="Province" required>
-                            
-                            <input class="form-format w-100 my-2 p-1" v-model="zipcode" type="number" placeholder="Zipcode" required>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                <button type="submit" class="btn register-btn">Register</button>
-            </div>
-        </form>
         </div>
     </div>
 </div>
@@ -102,57 +123,29 @@ export default {
 
 <style scoped>
 
-.header-btn {
-    border: none;
-}
+    .signup-container{
+        width: 900px;
+    }
 
-.register-btn{
-    background-color: #9F9A96;
-    color: white;
-}
+    .form-format{
+        font-family: Inter;
+        font-style: normal;
+        font-weight: lighter;
+        color: #000000;
+        background-color: #f6f6f6;
+        border-color: #ececec;
+    }
 
-.form-format{
-    font-family: Inter;
-    font-style: normal;
-    font-weight: lighter;
-    color: #9F9A96;
-    background-color: #f6f6f6;
-    border: #ececec;
-    font-size: 0.8rem;
-}
+    .btn-container{
+        width: 335px;
+    }
 
-.show-btn{
-    border: #ececec;
-    border-left: none;
-}
+    .btn-format {
+        background: #9F9A96;
+        border-radius: 8px;
+    }
 
-.validation-text{
-    font-family: Inter;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 14px;
-    line-height: 20px; /* identical to box height, or 143% */
-    letter-spacing: 0.2px;
-    color: #DC3915;
-}
-
-.modal-text {
-    font-family: Inter;
-    font-style: normal;
-    font-weight: 300;
-    font-size: 20.5px;
-}
-
-.btn-container{
-    width: 335px;
-}
-
-.button {
-    background: #9F9A96;
-    border-radius: 8px;
-}
-
-form, input, label, p {
-    color: black !important;
-}
+    .text-size {
+        font-size: 16px;
+    }
 </style>
